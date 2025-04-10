e985ef7fa5265c44e1ba1c7be4bb4a7dd16152d5
Simone De Cristofaro <simone.decristofaro@movesion.com>
2025-04-10 14:41:15 +0200

    -m

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -6,12 +6,15 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'wedding-sc-fe';
   isMenuOpen = false;
   currentYear = 2025;
+
+  private touchStartX = 0;
+  private touchEndX = 0;
 
   toggleMenu(): void {
     this.isMenuOpen = !this.isMenuOpen;
@@ -19,5 +22,28 @@ export class AppComponent {
 
   closeMenu(): void {
     this.isMenuOpen = false;
+  }
+
+  openMenu(): void {
+    this.isMenuOpen = true;
+  }
+
+  @HostListener('touchstart', ['$event'])
+  onTouchStart(event: TouchEvent): void {
+    this.touchStartX = event.changedTouches[0].screenX;
+  }
+
+  @HostListener('touchend', ['$event'])
+  onTouchEnd(event: TouchEvent): void {
+    this.touchEndX = event.changedTouches[0].screenX;
+    this.handleSwipeGesture();
+  }
+
+  private handleSwipeGesture(): void {
+    const swipeThreshold = 50; // Minimum swipe distance in pixels
+    if (this.touchEndX - this.touchStartX > swipeThreshold) {
+      // Swipe right: Close the menu
+      this.closeMenu();
+    }
   }
 }
