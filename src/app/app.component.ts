23e111c78a528f66e01a239b60d385da6d1b4b0d
Simone De Cristofaro <simone.decristofaro@movesion.com>
2025-04-15 09:30:44 +0200

    Implementing toggle theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,27 @@
-import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
 import { Router, RouterLink, RouterOutlet } from '@angular/router';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { StorageService } from './services/storage.service';
-import { NgIf } from '@angular/common';
+import { NgClass, NgIf, NgSwitch } from '@angular/common';
+import { Theme } from './models/theme';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, TranslateModule, NgIf],
+  imports: [RouterOutlet, RouterLink, TranslateModule, NgIf, NgSwitch, NgClass],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  Theme = Theme; // expose enum to template if needed
+
   title = 'wedding-sc-fe';
   isMenuOpen = false;
   currentYear = new Date().getFullYear();
   currentLanguage: string;
   isHeaderHidden = false; // Track whether the header is hidden
   lastScrollTop = 0; // Track the last scroll position
+  currentTheme: Theme = Theme.Light;
+  themeDropdownOpen = false;
 
   private touchStartX = 0;
   private touchEndX = 0;
@@ -36,6 +41,13 @@ export class AppComponent {
     translateService.use(this.currentLanguage);
   }
 
+  ngOnInit(): void {
+    const savedTheme = this.storageService.get('theme') as Theme | null;
+    this.currentTheme = savedTheme ?? Theme.Light;
+    this.applyTheme(this.currentTheme);
+  }
+  
+
   @ViewChild('languageDropdown') languageDropdownRef!: ElementRef;
 
   languageDropdownOpen = false;
@@ -50,6 +62,26 @@ export class AppComponent {
     this.translateService.use(lang);
     localStorage.setItem('language', lang);
   }
+
+  toggleThemeDropdown(): void {
+    this.themeDropdownOpen = !this.themeDropdownOpen;
+  }
+  
+  selectTheme(theme: Theme): void {
+    this.currentTheme = theme;
+    this.themeDropdownOpen = false;
+    this.storageService.set('theme', theme);
+    this.applyTheme(theme);
+  }
+  
+  applyTheme(theme: Theme): void {
+    //todo_here
+    // const root = document.documentElement;
+    // root.setAttribute('data-theme', theme); // optionally used in CSS
+  }
+
+
+
 
   toggleMenu(): void {
     this.isMenuOpen = !this.isMenuOpen;
