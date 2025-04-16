3727d740586f05f6260a538e288af440c1a1fe96
sidec15 <simone.decristofaro85@gmail.com>
2025-04-16 17:11:56 +0200

    Creating church page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { OurStoryComponent } from './pages/our-story/our-story.component';
+import { ChurchComponent } from './pages/church/church.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent }, // Default route
   { path: 'our-story', component: OurStoryComponent }, // Our Story route
-  { path: 'parallax', component: OurStoryComponent }
+  { path: 'church', component: ChurchComponent }, // Church route
 ];
---
+++ src/app/pages/church/church.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-church',
+  imports: [],
+  templateUrl: './church.component.html',
+  styleUrl: './church.component.scss'
+})
+export class ChurchComponent {
+
+}
