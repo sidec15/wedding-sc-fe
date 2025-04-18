3fd94a3c9e538775023ce27ffb6338915bc9df5a
sidec15 <simone.decristofaro85@gmail.com>
2025-04-18 02:10:52 +0200

    Adding lenis

--- src/app/pages/church/church.component.ts
+++ src/app/pages/church/church.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, OnDestroy, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import Lenis from 'lenis';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-church',
+  standalone: true,
   imports: [TranslateModule],
   templateUrl: './church.component.html',
-  styleUrl: './church.component.scss'
+  styleUrls: ['./church.component.scss']
 })
-export class ChurchComponent {
+export class ChurchComponent implements AfterViewInit, OnDestroy {
+  private lenis!: Lenis;
+  private rafId = 0;
+  private isBrowser: boolean;
 
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
+    this.isBrowser = isPlatformBrowser(this.platformId);
+  }
+
+  ngAfterViewInit(): void {
+    if (!this.isBrowser) return;
+
+    this.lenis = new Lenis({
+      duration: 1.5,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+    });
+
+    const raf = (time: number) => {
+      this.lenis.raf(time);
+      this.rafId = requestAnimationFrame(raf);
+    };
+
+    this.rafId = requestAnimationFrame(raf);
+  }
+
+  ngOnDestroy(): void {
+    if (this.isBrowser) {
+      cancelAnimationFrame(this.rafId);
+      this.lenis.destroy();
+    }
+  }
 }
