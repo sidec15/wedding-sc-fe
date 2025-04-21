e1fe8bd614bd0b5b266a346cb928ebc1fe007a5c
sidec15 <simone.decristofaro85@gmail.com>
2025-04-21 19:03:03 +0200

    adjusted church

--- src/app/pages/church/components/intro/intro.component.ts
+++ src/app/pages/church/components/intro/intro.component.ts
@@ -7,7 +7,7 @@ import {
   ViewChild,
 } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
-import { Subscription } from 'rxjs';
+import { Subscription, interval } from 'rxjs';
 import { EventService } from '../../services/event.service';
 import { PlatformService } from '../../../../services/platform.service';
 
@@ -15,17 +15,25 @@ import { PlatformService } from '../../../../services/platform.service';
   selector: 'app-intro',
   imports: [TranslateModule],
   templateUrl: './intro.component.html',
-  styleUrl: './intro.component.scss',
+  styleUrls: ['./intro.component.scss'],
 })
 export class IntroComponent implements AfterViewInit, OnInit, OnDestroy {
   private scrollEventSubscription!: Subscription;
-
   private heroHeight = 300; // fallback value
+  // private imageChangeIntervalSubscription!: Subscription;
 
   @ViewChild('heroOverlay', { static: false })
   heroOverlayRef!: ElementRef<HTMLElement>;
   @ViewChild('heroSection', { static: false })
   heroSectionRef!: ElementRef<HTMLElement>;
+
+  // // Array of hero images
+  // heroImages: string[] = [
+  //   '/images/church/intro/church-02.jpg',
+  //   '/images/church/intro/church-01.jpg',
+  //   '/images/church/intro/church-03.jpg',
+  // ];
+  // currentImageIndex = 0;
 
   constructor(
     private eventService: EventService,
@@ -42,6 +50,8 @@ export class IntroComponent implements AfterViewInit, OnInit, OnDestroy {
 
   ngOnInit(): void {
     if (!this.platformService.isBrowser()) return;
+
+    // Subscribe to scroll events
     this.scrollEventSubscription = this.eventService.scrollEvent$.subscribe(
       (scrollY) => {
         if (this.heroOverlayRef) {
@@ -49,11 +59,15 @@ export class IntroComponent implements AfterViewInit, OnInit, OnDestroy {
         }
       }
     );
+
+    // Start the image change timer
+    // this.startImageChangeTimer();
   }
 
   ngOnDestroy(): void {
     if (this.platformService.isBrowser()) {
       this.scrollEventSubscription?.unsubscribe();
+      // this.imageChangeIntervalSubscription?.unsubscribe();
     }
   }
 
@@ -67,4 +81,18 @@ export class IntroComponent implements AfterViewInit, OnInit, OnDestroy {
     el.style.opacity = `${opacity}`;
     el.style.transform = `translateY(${translateY}px)`;
   }
+
+  // private startImageChangeTimer(): void {
+  //   // Change the image every 5 seconds
+  //   this.imageChangeIntervalSubscription = interval(5000).subscribe(() => {
+  //     this.currentImageIndex =
+  //       (this.currentImageIndex + 1) % this.heroImages.length;
+
+  //     // Update the hero image
+  //     const heroSection = this.heroSectionRef?.nativeElement;
+  //     if (heroSection) {
+  //       heroSection.style.backgroundImage = `url('${this.heroImages[this.currentImageIndex]}')`;
+  //     }
+  //   });
+  // }
 }
