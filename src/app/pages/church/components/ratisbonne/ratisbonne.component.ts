a4a47283461cf7b7e742772c536aec42363854de
sidec15 <simone.decristofaro85@gmail.com>
2025-05-04 01:17:58 +0200

    starting ratisbonne section

---
+++ src/app/pages/church/components/ratisbonne/ratisbonne.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ratisbonne',
+  imports: [],
+  templateUrl: './ratisbonne.component.html',
+  styleUrl: './ratisbonne.component.scss'
+})
+export class RatisbonneComponent {
+
+}
