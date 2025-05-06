bd41a6b4a098f0572fc70c298e7f8865442920b0
sidec15 <simone.decristofaro85@gmail.com>
2025-05-06 17:33:32 +0200

    implementing miraculous medal section

--- src/app/pages/church/church.component.ts
+++ src/app/pages/church/church.component.ts
@@ -3,17 +3,17 @@ import {
 } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { IntroComponent } from './components/intro/intro.component';
-import { EventService } from '../../services/event.service';
 import { OriginsComponent } from './components/origins/origins.component';
 import { RingScrollComponent } from '../../components/ring-scroll/ring-scroll.component';
 import { AngelsComponent } from './components/angels/angels.component';
 import { MiracleComponent } from './components/miracle/miracle.component';
 import { RatisbonneComponent } from "./components/ratisbonne/ratisbonne.component";
+import { MedalComponent } from "./components/medal/medal.component";
 
 @Component({
   selector: 'app-church',
   standalone: true,
-  imports: [TranslateModule, IntroComponent, OriginsComponent, RingScrollComponent, AngelsComponent, MiracleComponent, RatisbonneComponent],
+  imports: [TranslateModule, IntroComponent, OriginsComponent, RingScrollComponent, AngelsComponent, MiracleComponent, RatisbonneComponent, MedalComponent],
   templateUrl: './church.component.html',
   styleUrls: ['./church.component.scss'],
 })
---
+++ src/app/pages/church/components/medal/medal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-medal',
+  imports: [TranslateModule],
+  templateUrl: './medal.component.html',
+  styleUrl: './medal.component.scss'
+})
+export class MedalComponent {
+
+}
