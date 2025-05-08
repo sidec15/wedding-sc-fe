22f874536192041ed753a181f024a7286416753d
sidec15 <simone.decristofaro85@gmail.com>
2025-05-08 13:23:35 +0200

    implementing minimi section

--- src/app/pages/church/components/minimi/minimi.component.ts
+++ src/app/pages/church/components/minimi/minimi.component.ts
@@ -1,30 +1,30 @@
 import { NgStyle } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-minimi',
-  imports: [TranslateModule, NgStyle],
+  imports: [TranslateModule],
   templateUrl: './minimi.component.html',
   styleUrl: './minimi.component.scss'
 })
-export class MinimiComponent implements OnInit {
+export class MinimiComponent implements AfterViewInit, OnDestroy {
 
   slides = [
     {
-      imageUrl: '/images/origins/origins-01.png',
+      imageUrl: '/images/church/origins/origins-01.png',
       text: 'The Sanctuary of San Francesco di Paola in Calabria, spiritual heart of the Order of Minims.'
     },
     {
-      imageUrl: '/images/origins/origins-03.png',
+      imageUrl: '/images/church/origins/origins-03.png',
       text: 'The Order of Minims lives in humility, penance, and charity, following the path of their founder.'
     },
     {
-      imageUrl: '/images/origins/origins-03.png',
+      imageUrl: '/images/church/origins/origins-03.png',
       text: 'San Francesco di Paola, a man of miracles and peace, called all to a life of simplicity and love.'
     },
     {
-      imageUrl: '/images/origins/origins-04.png',
+      imageUrl: '/images/church/origins/origins-04.png',
       text: 'In silence and contemplation, the Frati Minimi dedicate their lives to prayer and service.'
     }
   ];
@@ -32,11 +32,12 @@ export class MinimiComponent implements OnInit {
   currentSlideIndex = 0;
   currentSlide = this.slides[0];
 
-  ngOnInit(): void {
-    // setInterval(() => {
-    //   this.currentSlideIndex = (this.currentSlideIndex + 1) % this.slides.length;
-    //   this.currentSlide = this.slides[this.currentSlideIndex];
-    // }, 7000); // Change every 7 seconds
+  ngAfterViewInit(): void {
+    // throw new Error('Method not implemented.');
+  }
+
+  ngOnDestroy(): void {
+    // throw new Error('Method not implemented.');
   }
 
 }
