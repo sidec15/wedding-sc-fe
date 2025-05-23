4bc6369127c491f69a0fcb2f6d4e3571c0ea9c74
sidec15 <simone.decristofaro85@gmail.com>
2025-05-23 23:57:44 +0200

    Implementing mobile

--- src/app/pages/church/components/miracle/miracle.component.ts
+++ src/app/pages/church/components/miracle/miracle.component.ts
@@ -1,5 +1,6 @@
 import {
   AfterViewInit,
+  ChangeDetectorRef,
   Component,
   ElementRef,
   OnDestroy,
@@ -8,60 +9,28 @@ import {
 } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { ParallaxImageComponent } from '../../../../components/parallax-image/parallax-image.component';
-import { EventService, ScrollEvent } from '../../../../services/event.service';
 import { PlatformService } from '../../../../services/platform.service';
-import { Subscription } from 'rxjs';
 import { NgClass, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-miracle',
-  standalone: true,
   imports: [TranslateModule, ParallaxImageComponent, NgClass, NgIf],
   templateUrl: './miracle.component.html',
   styleUrl: './miracle.component.scss',
 })
-export class MiracleComponent implements OnInit, AfterViewInit, OnDestroy {
-  private scrollSub!: Subscription;
+export class MiracleComponent implements AfterViewInit {
+  isMobile: boolean = false;
 
-  currentParagraph = 0;
-  isMobile = false;
+  constructor(private platformService: PlatformService) {}
 
-  @ViewChild('miracleSection', { static: true }) sectionRef!: ElementRef;
-
-  constructor(
-    private eventService: EventService,
-    private platformService: PlatformService
-  ) {}
-
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
     this.isMobile = this.platformService.isMobile();
   }
 
-  ngAfterViewInit(): void {
-    if (!this.platformService.isMobile()) return;
-
-    this.scrollSub = this.eventService.scrollEvent$.subscribe((e) => {
-      // this.onScroll(e);
-    });
-  }
-
-  ngOnDestroy(): void {
-    this.scrollSub?.unsubscribe();
-  }
-
-  private onScroll(scroll: ScrollEvent): void {
-    const rect = this.sectionRef.nativeElement.getBoundingClientRect();
-    const windowHeight = window.innerHeight;
-
-    const visibleRatio = (windowHeight - rect.top) / rect.height;
-    const clampedRatio = Math.max(0, Math.min(1, visibleRatio));
-
-    if (clampedRatio < 0.75) {
-      this.currentParagraph = 0;
-    } else if (clampedRatio < 0.9) {
-      this.currentParagraph = 1;
-    } else {
-      this.currentParagraph = 2;
-    }
+  get imageSrc(): string {
+    return !this.isMobile
+      ? '/images/church/miracle/miracle-02.jpg'
+      : '/images/church/miracle/miracle-09.jpg';
   }
 }
+
