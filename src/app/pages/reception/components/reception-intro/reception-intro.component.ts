841630e3d6915d86fcc81ce686f9939fc15c9040
sidec15 <simone.decristofaro85@gmail.com>
2025-05-31 15:26:37 +0200

    Implementing reception

--- src/app/pages/reception/components/reception-intro/reception-intro.component.ts
+++ src/app/pages/reception/components/reception-intro/reception-intro.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { NgStyle } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-reception-intro',
-  imports: [],
+  imports: [TranslateModule, NgStyle],
   templateUrl: './reception-intro.component.html',
-  styleUrl: './reception-intro.component.scss'
+  styleUrl: './reception-intro.component.scss',
 })
 export class ReceptionIntroComponent {
+  @Input() images: string[] = [
+    '/images/reception/intro/intro-01.png',
+  ];
+  currentImageIndex = 0;
 
+  get imageUrl(): string {
+    return `url(${this.images[this.currentImageIndex]})`;
+  }
+
+  // Optional: automatic switch
+  ngOnInit(): void {
+    // setInterval(() => {
+    //   this.currentImageIndex =
+    //     (this.currentImageIndex + 1) % this.images.length;
+    // }, 8000);
+  }
 }
