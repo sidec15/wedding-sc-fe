5bab6b4bf425d8c03d1d6bc08f80909bf159e2fc
sidec15 <simone.decristofaro85@gmail.com>
2025-06-05 17:13:09 +0200

    Implementing reception

--- src/app/pages/reception/components/reception-location/reception-location.component.ts
+++ src/app/pages/reception/components/reception-location/reception-location.component.ts
@@ -1,5 +1,5 @@
 import { NgForOf } from '@angular/common';
-import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { interval, Subscription } from 'rxjs';
 import { HeaderBgFillObserverDirective } from '../../../../directives/header-bg-fill-observer.directive';
@@ -22,6 +22,26 @@ export class ReceptionLocationComponent implements AfterViewInit, OnDestroy {
   currentImageIndex = 0;
 
   private imageRotationSub!: Subscription;
+
+
+@ViewChild('p1', { static: true }) p1!: ElementRef;
+@ViewChild('p2', { static: true }) p2!: ElementRef;
+@ViewChild('p3', { static: true }) p3!: ElementRef;
+
+@HostListener('window:scroll', [])
+onScroll(): void {
+  const triggerPoint = window.innerHeight * 0.85;
+
+  [this.p1, this.p2, this.p3].forEach((el) => {
+    const top = el.nativeElement.getBoundingClientRect().top;
+    if (top < triggerPoint) {
+      el.nativeElement.classList.add('visible');
+    }
+  });
+}
+
+
+
 
   constructor(private platformService: PlatformService) {}
 
