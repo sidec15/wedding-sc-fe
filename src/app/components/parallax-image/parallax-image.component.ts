dbf21c4840aef76f7f301de793e023884050dd9e
sidec15 <simone.decristofaro85@gmail.com>
2025-06-05 09:32:27 +0200

    fixing parallax image

--- src/app/components/parallax-image/parallax-image.component.ts
+++ src/app/components/parallax-image/parallax-image.component.ts
@@ -23,14 +23,12 @@ export class ParallaxImageComponent implements AfterViewInit, OnDestroy {
   alt = input<string>('');
   containerHeight = input('500px');
   imageHeight = input('650px');
-  speedFactor = input(0.1);
+  speedFactor = input(0.05);
 
   @ViewChild('image', { static: true }) imageRef!: ElementRef;
   @ViewChild('wrapper', { static: true }) wrapperRef!: ElementRef;
 
-  private parallaxOffset = 0;
   private scrollSubscription!: Subscription;
-  private positionYInViewPort!: string;
 
   constructor(
     private eventService: EventService,
@@ -41,7 +39,7 @@ export class ParallaxImageComponent implements AfterViewInit, OnDestroy {
     this.scrollSubscription = this.eventService.scrollEvent$
       .pipe(throttleTime(16)) // Throttle to 60 FPS
       .subscribe((e: ScrollEvent) => {
-        this.checkVisibility(e);
+        // this.checkVisibility(e);
         this.animate();
       });
   }
@@ -50,38 +48,23 @@ export class ParallaxImageComponent implements AfterViewInit, OnDestroy {
     this.scrollSubscription?.unsubscribe();
   }
 
-animate(): void {
-  if (!this.platformService.isPlatformReady()) return;
-  if (this.positionYInViewPort !== 'visible') return;
-
-  const wrapper = this.wrapperRef.nativeElement as HTMLElement;
-  const img = this.imageRef.nativeElement as HTMLElement;
-
-  const rect = wrapper.getBoundingClientRect();
-  const windowHeight = window.innerHeight;
-
-  // Parallax base: how far from center of viewport
-  const distanceFromCenter = rect.top + rect.height / 2 - windowHeight / 2;
-
-  // This value should be smaller than 1 (e.g. 0.2) to slow down image movement
-  const translateY = distanceFromCenter * this.speedFactor();
-
-  img.style.transform = `translateY(${translateY}px)`;
-}
-
-
-  checkVisibility(e: ScrollEvent) {
+  animate(): void {
+    if (!this.platformService.isPlatformReady()) return;
     const wrapper = this.wrapperRef.nativeElement as HTMLElement;
-    this.positionYInViewPort =
-      this.platformService.positionYInViewport(wrapper);
-    if (
-      this.positionYInViewPort === 'below' ||
-      this.positionYInViewPort === 'above'
-    ) {
-      // Reset parallax offset when the element is out of view
-      this.parallaxOffset = 0;
-      const img = this.imageRef.nativeElement as HTMLElement;
-      img.style.transform = `translateY(0px)`;
-    }
+
+    if(!this.platformService.isVisible(wrapper)) return;
+
+    const img = this.imageRef.nativeElement as HTMLElement;
+
+    const rect = wrapper.getBoundingClientRect();
+    const windowHeight = window.innerHeight;
+
+    // Parallax base: how far from center of viewport
+    const distanceFromCenter = rect.top + rect.height / 2 - windowHeight / 2;
+
+    // This value should be smaller than 1 (e.g. 0.2) to slow down image movement
+    const translateY = distanceFromCenter * this.speedFactor();
+
+    img.style.transform = `translateY(${translateY}px)`;
   }
 }
