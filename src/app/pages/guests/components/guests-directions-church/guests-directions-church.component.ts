f70c0b70c7506a1621daae62d0c7b3b7a090fcd1
sidec15 <simone.decristofaro85@gmail.com>
2025-06-10 01:45:43 +0200

    Implementing guests section

--- src/app/pages/guests/components/guests-directions-church/guests-directions-church.component.ts
+++ src/app/pages/guests/components/guests-directions-church/guests-directions-church.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-guests-directions-church',
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './guests-directions-church.component.html',
   styleUrl: './guests-directions-church.component.scss'
 })
