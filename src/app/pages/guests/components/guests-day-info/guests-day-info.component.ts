277bc1323cb2193086528f4e22033316cfd3296b
sidec15 <simone.decristofaro85@gmail.com>
2025-06-10 01:10:35 +0200

    Implementing guests section

---
+++ src/app/pages/guests/components/guests-day-info/guests-day-info.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-guests-day-info',
+  imports: [TranslateModule],
+  templateUrl: './guests-day-info.component.html',
+  styleUrl: './guests-day-info.component.scss'
+})
+export class GuestsDayInfoComponent {}
--- src/app/pages/guests/guests.component.ts
+++ src/app/pages/guests/guests.component.ts
@@ -4,6 +4,7 @@ import { GuestsDirectionsChurchComponent } from './components/guests-directions-
 import { GuestsDirectionsReceptionComponent } from './components/guests-directions-reception/guests-directions-reception.component';
 import { GuestsContactFormComponent } from './components/guests-contact-form/guests-contact-form.component';
 import { GuestsSaveTheDateComponent } from './components/guests-save-the-date/guests-save-the-date.component';
+import { GuestsDayInfoComponent } from './components/guests-day-info/guests-day-info.component';
 
 @Component({
   selector: 'app-guests',
@@ -12,7 +13,8 @@ import { GuestsSaveTheDateComponent } from './components/guests-save-the-date/gu
     GuestsDirectionsChurchComponent,
     GuestsDirectionsReceptionComponent,
     GuestsSaveTheDateComponent,
-    GuestsContactFormComponent
+    GuestsContactFormComponent,
+    GuestsDayInfoComponent
   ],
   templateUrl: './guests.component.html',
   styleUrl: './guests.component.scss',
