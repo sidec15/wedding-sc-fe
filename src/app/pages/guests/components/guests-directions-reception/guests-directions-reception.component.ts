d56827969aec3d6056a9c4fa5426875554042e26
sidec15 <simone.decristofaro85@gmail.com>
2025-06-10 23:10:34 +0200

    Implementing guests section

--- src/app/pages/guests/components/guests-directions-reception/guests-directions-reception.component.ts
+++ src/app/pages/guests/components/guests-directions-reception/guests-directions-reception.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-guests-directions-reception',
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './guests-directions-reception.component.html',
   styleUrl: './guests-directions-reception.component.scss'
 })
