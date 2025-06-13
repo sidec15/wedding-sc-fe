243768b9941ca35aa2b9ce3ec05c591b16e2e735
sidec15 <simone.decristofaro85@gmail.com>
2025-06-13 10:36:20 +0200

    Implementing guests section

--- src/app/pages/guests/components/guests-save-the-date/guests-save-the-date.component.ts
+++ src/app/pages/guests/components/guests-save-the-date/guests-save-the-date.component.ts
@@ -1,7 +1,7 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { EventService } from '../../../../services/event.service';
 import { ImageFocusComponent } from '../../../../components/image-focus/image-focus.component';
-import { TranslateModule } from '@ngx-translate/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { CommonModule } from '@angular/common';
 import 'add-to-calendar-button';
 
@@ -22,29 +22,14 @@ export class GuestsSaveTheDateComponent {
   googleUrl = '';
   microsoftUrl = '';
   appleUrl = '';
+  currentLang: string;
 
-  calendarConfig = {
-    name: 'Matrimonio Chiara & Simone',
-    description: 'Cerimonia di matrimonio',
-    startDate: '2025-10-10',
-    startTime: '15:00',
-    endTime: '17:00',
-    timeZone: 'Europe/Rome',
-    location: "Chiesa Sant'Andrea delle Fratte, Roma",
-    options: [
-      'Apple',
-      'Google',
-      'iCal',
-      'Microsoft365',
-      'Outlook.com',
-      'Yahoo',
-    ],
-    buttonStyle: 'round',
-    listStyle: 'modal',
-    language: 'it',
-  };
-
-  constructor(private eventService: EventService) {}
+  constructor(private eventService: EventService, private translate: TranslateService) {
+    this.currentLang = this.translate.currentLang;
+    this.translate.onLangChange.subscribe(event => {
+      this.currentLang = event.lang;
+    });
+  }
 
   openGallery(): void {
     this.isGalleryOpen = true;
