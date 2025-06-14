301eda96ce98bf953b71b4177f4c1d2768dc02d6
sidec15 <simone.decristofaro85@gmail.com>
2025-06-14 22:59:54 +0200

    Migrting to angular 20

--- src/app/pages/guests/components/guests-contact-form/guests-contact-form.component.ts
+++ src/app/pages/guests/components/guests-contact-form/guests-contact-form.component.ts
@@ -1,11 +1,42 @@
 import { Component } from '@angular/core';
+import { CommonModule, NgIf } from '@angular/common';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-guests-contact-form',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule, TranslateModule, NgIf],
   templateUrl: './guests-contact-form.component.html',
-  styleUrl: './guests-contact-form.component.scss'
+  styleUrls: ['./guests-contact-form.component.scss']
 })
 export class GuestsContactFormComponent {
+  contactForm: FormGroup;
+  submitted = false;
+  maxMessageLength = 1000;
 
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      name: [''],
+      surname: [''],
+      phone: [''],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', [Validators.required, Validators.maxLength(this.maxMessageLength)]],
+      privacyConsent: [false, Validators.requiredTrue],
+      // Honeypot field
+      website: ['']
+    });
+  }
+
+  onSubmit() {
+    if (this.contactForm.get('website')?.value) {
+      // If honeypot is filled, silently reject
+      return;
+    }
+
+    if (this.contactForm.valid) {
+      this.submitted = true;
+      // TODO: Implement form submission
+      console.log(this.contactForm.value);
+    }
+  }
 }
