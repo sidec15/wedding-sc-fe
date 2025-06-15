34667530da055853604325f02bfede810a913d8d
sidec15 <simone.decristofaro85@gmail.com>
2025-06-16 00:08:00 +0200

    Migrting to angular 20

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { OurStoryComponent } from './pages/our-story/our-story.component';
 import { ChurchComponent } from './pages/church/church.component';
 import { ReceptionComponent } from './pages/reception/reception.component';
 import { GuestsComponent } from './pages/guests/guests.component';
+import { PrivacyPolicyComponent } from './pages/privacy-policy/privacy-policy.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent }, // Default route
@@ -11,5 +12,6 @@ export const routes: Routes = [
   { path: 'church', component: ChurchComponent }, // Church route
   { path: 'reception', component: ReceptionComponent }, // Reception route
   { path: 'guests', component: GuestsComponent }, // Guests route
+  { path: 'privacy-policy', component: PrivacyPolicyComponent }, // Privacy Policy route
   { path: '**', redirectTo: '' } // Wildcard route to redirect to home
 ];
---
+++ src/app/pages/privacy-policy/privacy-policy.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-privacy-policy',
+  imports: [TranslateModule],
+  templateUrl: './privacy-policy.component.html',
+  styleUrl: './privacy-policy.component.scss'
+})
+export class PrivacyPolicyComponent {
+
+}
