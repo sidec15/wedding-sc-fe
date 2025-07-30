ca3207923f1483395af4fe494ed24f8af2ccb8db
sidec15 <simone.decristofaro85@gmail.com>
2025-07-30 10:41:22 +0200

    Implementing different components for desktop and mobile

--- src/app/pages/our-story/services/cards.service.ts
+++ src/app/pages/our-story/services/cards.service.ts
@@ -8,44 +8,74 @@ export class CardsService {
       type: 'intro',
       title: 'our_story.intro.title',
       description: 'our_story.intro.description',
-
-      position: 'current',
-      status: 'visible',
     },
     {
       type: 'card',
-      title: 'our_story.cards.0.title',
-      description: 'our_story.cards.0.description',
-      image: '/images/our-story/our-story_19860401.jpg',
-      comics: ['/images/comics/comics-04.png', '/images/comics/comics-01.png'],
-
-      position: 'after',
-      status: 'hidden',
-      textPosition: 'left',
-
+      title: 'our_story.cards.chiara_neonata.title',
+      description: 'our_story.cards.chiara_neonata.description',
+      image: '/images/our-story/our-story-chiara_neonata.jpg',
+      comics: ['/images/comics/comics-chiara_neonata.png'],
     },
     {
       type: 'card',
-      title: 'our_story.cards.1.title',
-      description: 'our_story.cards.1.description',
-      image: '/images/our-story/our-story_19870801.jpg',
-      comics: ['/images/comics/comics-04.png'],
-
-      position: 'after',
-      status: 'hidden',
-      textPosition: 'left',
+      title: 'our_story.cards.chiara_piccola_famiglia.title',
+      description: 'our_story.cards.chiara_piccola_famiglia.description',
+      image: '/images/our-story/our-story-chiara_piccola_famiglia.jpg',
+      comics: ['/images/our-story/our-story-comics-chiara_piccola_famiglia.png'],
+    },
+    {
+      type: 'card',
+      title: 'our_story.cards.simone_piccolo_calcio.title',
+      description: 'our_story.cards.simone_piccolo_calcio.description',
+      image: '/images/our-story/our-story-simone_piccolo_calcio.jpg',
+      comics: ['/images/our-story/our-story-comics-simone_piccolo_calcio.png'],
+    },
+    {
+      type: 'card',
+      title: 'our_story.cards.chiara_piccola_primavera.title',
+      description: 'our_story.cards.chiara_piccola_primavera.description',
+      image: '/images/our-story/our-story-chiara_piccola_primavera.jpg',
+    },
+    {
+      type: 'card',
+      title: 'our_story.cards.simone_venezia.title',
+      description: 'our_story.cards.simone_venezia.description',
+      image: '/images/our-story/our-story-simone_venezia.jpg',
+      comics: ['/images/our-story/our-story-comics-simone_venezia.png'],
+    },
+    {
+      type: 'card',
+      title: 'our_story.cards.chiara_piccola_natale.title',
+      description: 'our_story.cards.chiara_piccola_natale.description',
+      image: '/images/our-story/our-story-chiara_piccola_natale.jpg',
+      comics: ['/images/our-story/our-story-comics-chiara_piccola_natale.png'],
+    },
+    {
+      type: 'card',
+      title: 'our_story.cards.chiara_piccola_nonni.title',
+      description: 'our_story.cards.chiara_piccola_nonni.description',
+      image: '/images/our-story/our-story-chiara_piccola_nonni.jpg',
     },
     {
       type: 'outro',
       title: 'our_story.outro.title',
       description: 'our_story.outro.description',
-
-      position: 'after',
-      status: 'hidden',
     },
   ];
 
-  constructor() {}
+  constructor() {
+    this.cards[0].status = 'visible';
+    this.cards[0].position = 'current';
+
+    for (let i = 1; i < this.cards.length -1; i++) {
+      this.cards[i].status = 'hidden';
+      this.cards[i].position = 'after';
+      this.cards[i].textPosition = 'left';
+    }
+
+    this.cards[this.cards.length - 1].status = 'hidden';
+    this.cards[this.cards.length - 1].position = 'after';
+  }
 
   getCards(): Card[] {
     return this.cards;
